143ba35029ada739ca477af23ad17eede0e4e4a6
Otis Singfield <singfieldotis@gmail.com>
2021-02-27 21:01:45 -0500

    start using JS variables

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,10 @@
-window.alert("This is an alert! JavaScript is running!")
+var playerName = window.prompt("what is your robot's name?");
+// What is this?
+console.log(playerName);
+
+
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+fight();
